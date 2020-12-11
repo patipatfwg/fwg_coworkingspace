accb42d5de7148c44b5ad2b194fc78b00dab8e4f
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2020-12-11 21:36:10 +0700

    2136

--- src/views/pages/login/tmp/_Login.js
+++ src/views/pages/login/tmp/_Login.js
@@ -17,24 +17,18 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
-const Hi = () => {
-  return 'Hi';
-}
-
 const Login = () => {
-
-
   return (
-    <div className="c-app c-default-layout flex-row align-items-center bg-info">
+    <div className="c-app c-default-layout flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md="6">
+          <CCol md="8">
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
                   <CForm>
-                    <h1>Co-Working Space</h1>
-                    <p className="text-muted">Sign In with FWG Prompt</p>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
                     <CInputGroup className="mb-3">
                       <CInputGroupPrepend>
                         <CInputGroupText>
@@ -53,10 +47,22 @@ const Login = () => {
                     </CInputGroup>
                     <CRow>
                       <CCol xs="12">
-                        <CButton color="success" className="px-4">Login</CButton>
+                        <CButton color="primary" className="px-4">Login</CButton>
                       </CCol>
                     </CRow>
                   </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.</p>
+                    <Link to="/register">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
+                    </Link>
+                  </div>
                 </CCardBody>
               </CCard>
             </CCardGroup>
