7e2e52ebb9c88cac60e4c244a496647bf71c8720
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2020-12-17 18:15:38 +0700

    1815

--- api/create.php
+++ api/create.php
@@ -1,12 +1,37 @@
 <?php
 
-include("config.php");
+date_default_timezone_set("Asia/Bangkok");
+
+include("configmdc.php");
 
 if($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     $action = $_REQUEST["action"];
-    if($action=='create')
+    if($action =='create')
     {
-        
+        $employee_id = $_REQUEST["user_id"];
+        $booking_employee_start = $_REQUEST['booking_employee_start'];
+        $booking_employee_end = $_REQUEST['booking_employee_end'];
+        $booking_type_id = 1;
+        if($booking_type_id==2)
+        {
+            $booking_room_id = $_REQUEST['booking_room_id'];
+            $booking_zone_id = 0;
+            $booking_seat_id = 0;
+        }
+        else if($booking_type_id==1)
+        {
+            $booking_room_id = 0;
+            $booking_zone_id = $_REQUEST['booking_zone_id'];
+            $booking_seat_id = $_REQUEST['booking_seat_id'];
+        }
+        $booking_employee_status = "checkin";
+        $sql = "INSERT INTO booking_employee (employee_id,booking_employee_start,booking_employee_end,booking_type_id,booking_room_id,booking_zone_id,booking_seat_id,booking_employee_status,created_at,updated_at) VALUES ( '$employee_id','$booking_employee_start','$booking_employee_end','$booking_type_id',$booking_room_id,'$booking_zone_id','$booking_seat_id','$booking_employee_status', now(),now())";
+        $result = $mysqli->query($sql);
+        if( $result )
+        {
+            $myArray = ["Message"=>"Saved"];
+            echo json_encode($myArray);
+        }
     }
 }
--- api/read.php
+++ api/read.php
@@ -1,18 +1,22 @@
 <?php
 
-include("config.php");
+date_default_timezone_set("Asia/Bangkok");
+
+// Login 
+// Dashboard
+// Booking
 
 if($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     $action = $_REQUEST["action"];
     if($action=='login')
     {
+        include("config.php");
         $username = $_REQUEST["username"];
         $password = $_REQUEST["password"];
         $sql = "SELECT first_name_en,last_name_en,user_id FROM user LEFT JOIN employee ON user.id = employee.user_id WHERE user.activate = 1 AND user.id = '$username' AND user.password = '$password'";
         $result = $mysqli->query($sql);
         $count = mysqli_num_rows($result);
-
         if($count>0)
         {
             $row = $result->fetch_array();
@@ -26,7 +30,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
                 "lastname"=>$last_name_en,
                 "user_id"=>$user_id
             );
-            $result->close();
+            $mysqli->close();
         }
         else
         {
@@ -37,30 +41,66 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
                 "Message"=>$msg
             );
         }
-
-        echo json_encode($myArray);
-        $mysqli->close();        
+        echo json_encode($myArray);       
     }
-    else if($action=='create')
+    else if($action=='dashboard')
     {
-        $sql = "SELECT first_name_en,last_name_en FROM user LEFT JOIN employee ON user.id = employee.user_id WHERE user.activate = 1 AND user.id = '$username' AND user.password = '$password'";
-        $result = $mysqli->query($sql);
-        $count = mysqli_num_rows($result);
+        $code = 200;
+        
 
-        if($count>0)
-        {
-            $row = $result->fetch_array();
-            $code = 200;
-            $myArray = array(
-                "code"=>$code,
-                "type"=>"seat",
-                "zone"=>""
-            );
-            $result->close();
-        }
 
+        $msg = "Hi";
+        $myArray = array(
+            "code"=>$code,
+            "Message"=>$msg
+        );
         echo json_encode($myArray);
-        $mysqli->close(); 
+    }
+    else if($action=='booking')
+    {
+        $code = 200;
+        $type = "seat";
+        $zoneList = getZone();
+        $dashboard = getDashboard();
+        $myArray = array(
+            "code"=>$code,
+            "type"=>$type,
+            "zone"=>$zoneList,
+            "dashboard"=>$dashboard
+        );
+        echo json_encode($myArray);
     }
 }
+
+function getZone()
+{
+    include("configmdc.php");
+    $sql_seat = "SELECT booking_zone_title,booking_seat_amount FROM booking_seat LEFT JOIN booking_zone ON booking_seat.booking_zone_id = booking_zone.booking_zone_id";
+    $result_seat = $mysqli->query($sql_seat);
+    $count_seat = mysqli_num_rows($result_seat);
+    $zoneList = [];
+    if($count_seat>0)
+    {
+        while($rowSeat = $result_seat->fetch_array())
+        {
+            $booking_zone_title = $rowSeat["booking_zone_title"];
+            $booking_seat_amount = $rowSeat["booking_seat_amount"];
+            $zoneList[] = array(
+                $booking_zone_title => intval($booking_seat_amount)
+            );
+        }
+    }
+    $mysqli->close(); 
+    return $zoneList;
+}
+
+function getDashboard()
+{
+    include("configmdc.php");
+
+    $dashboard = [];
+
+    return $dashboard;
+}
+
 ?>
